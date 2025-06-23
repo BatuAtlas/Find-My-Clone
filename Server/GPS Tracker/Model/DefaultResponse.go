8547838fb5f509fc69dcc8a0611c118c6d163161
Luz Eterna <anelastar42@gmail.com>
2025-06-23 16:11:13 +0300

    Response Parser Modified

--- Server/GPS Tracker/Model/DefaultResponse.go
+++ Server/GPS Tracker/Model/DefaultResponse.go
@@ -1,34 +1,24 @@
 package model
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 type Response struct {
 	Status   bool        `json:"status"`
 	Response interface{} `json:"response"`
 }
 
-type Error struct {
-	Message   string `json:"message"`
-	ErrorCode int    `json:"error"`
-}
-
-func ParseResponse(status bool, response interface{}) ([]byte, error) {
+func ParseResponse(status bool, response interface{}) []byte {
 	data, err := json.Marshal(Response{Status: status, Response: response})
 	if err != nil {
-		return nil, err
+		data2 := ParseResponse(false, fiber.Map{"message": "Json Handle Error", "errorcode": 48})
+		log.Println("Errcode 48 = " + err.Error())
+		return data2
 	}
 
-	return data, nil
-}
-
-//data, err := ParseResponse(status, err)
-func HandleResponse(data []byte, err error) (response []byte) {
-	if err != nil {
-		data2, _ := ParseResponse(false, Error{Message: "Json Handle Error", ErrorCode: 48})
-		response = data2
-		return
-	}
-
-	response = data
-	return
+	return data
 }
