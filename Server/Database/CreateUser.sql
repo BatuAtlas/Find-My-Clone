7f2f4a8ec2a187549f59e40bdf45fd06a11d670d
Luz Eterna <anelastar42@gmail.com>
2025-06-27 12:53:21 +0300

    Minor database fix

--- Server/Database/CreateUser.sql
+++ Server/Database/CreateUser.sql
@@ -1,6 +1,6 @@
 WITH new_user AS (
   INSERT INTO "User" ("nickname")
-  VALUES ('Selin')
+  VALUES ($1) 
   RETURNING "id"
 ),
 userinfo_insert AS (
@@ -13,5 +13,12 @@ auth_insert AS (
   SELECT "id", encode(gen_random_bytes(32), 'hex')
   FROM new_user
   RETURNING "token", "user"
+),
+usersettings_insert AS (
+  INSERT INTO "Usersettings"("user", "mail", "password") 
+  SELECT "id", $2, $3
+  FROM new_user
+  RETURNING "user"
 )
 SELECT "user" AS "id", "token" FROM auth_insert;
+
